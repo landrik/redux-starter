807c4751959ad607d9357c5ccd7f79baecf1beb7
landrik <landrik@gmail.com>
2018-05-09 18:06:20 +0100

    updated folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,19 @@
-// src/components/App.js
+// this component handles the App template used on every page.
+import React, {PropTypes} from 'react';
+import Header from './common/Header';
+
+class App extends React.Component{
+  render(){
+    return(
+      <div className="container-fluid">
+        <Header />
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.object.isRequired
+};
+export default App;
---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class AboutPage extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>About Page</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tristique, eros vitae dignissim dictum, metus massa elementum neque, sed congue nisl felis a libero. Vivamus porta, neque at ultrices vulputate, augue ipsum blandit nulla, sed malesuada neque ex ullamcorper orci. Integer auctor, ex at pulvinar blandit, massa nisi malesuada dolor, vitae accumsan nibh odio in ligula. Quisque eget sagittis purus, quis sodales arcu. Pellentesque hendrerit malesuada erat eu vehicula. Curabitur rutrum justo id arcu aliquet rhoncus. Fusce sit amet magna eu orci fermentum congue. Suspendisse posuere est odio, et hendrerit mauris rhoncus ut. Phasellus nec elit sem. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec mattis euismod ligula nec tempor. Sed sit amet nibh risus. Morbi urna risus, pulvinar sit amet diam sed, aliquet laoreet mi. Nam malesuada volutpat est, sed ullamcorper leo cursus quis. Nunc non turpis dignissim, auctor eros sit amet, condimentum arcu. Curabitur egestas ante ut varius varius.</p>
+        <p>Suspendisse semper tortor sed orci tempus, sed iaculis erat scelerisque. In lorem sapien, consectetur gravida laoreet vel, tincidunt eu lacus. In eget est ut arcu sodales aliquam. Donec tincidunt interdum erat, id sollicitudin diam vehicula id. Integer aliquam tortor ac lectus placerat, vel iaculis felis pharetra. Nam placerat eget ante at vehicula. Integer quis enim vitae sapien viverra aliquam at sed sapien. Pellentesque id aliquet nisl. Quisque consequat mi a interdum bibendum. Mauris vel ornare dui. Mauris auctor mi tortor, id eleifend elit ullamcorper vel. Quisque dictum turpis sed est iaculis efficitur. Sed sit amet elit purus.</p>
+        <p>Iteger finibus lacinia sem, mollis pellentesque nunc finibus vel. Aliquam sed ex leo. Maecenas posuere eu mauris a tempus. Proin id dolor eros. Nulla eleifend quam condimentum dui condimentum sagittis. Aenean ac elit turpis. Praesent et lacus sollicitudin, suscipit elit eu, venenatis libero. Sed condimentum lectus ut justo fermentum dictum. Donec id arcu eu neque placerat auctor. Vestibulum aliquet leo vel mauris vulputate rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel nisl tellus. Duis sagittis, elit ut elementum auctor, est neque tempus leo, tincidunt condimentum odio ante sit amet ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    );
+  }
+}
+export default AboutPage;
