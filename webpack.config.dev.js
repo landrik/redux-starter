7a8d885ff6df865b4c2381529694a4bb184596e5
landrik <landrik@gmail.com>
2018-05-10 17:44:50 +0100

    messed up

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -28,12 +28,10 @@ export default {
     loaders: [
       {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
       {test: /(\.css)$/, loaders: ['style', 'css']},
-      // {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-      // {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
-      // {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-      // {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
-      // {test: /\.css$/, loaders: ['style-loader', 'css-loader' ]},
-      {test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/, loader: 'url-loader?limit=100000'}
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
     ]
   }
 };
