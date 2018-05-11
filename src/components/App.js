cda057438d3c57f73ece666ee811d47fc59327a9
landrik <landrik@gmail.com>
2018-05-11 10:31:46 +0100

    updated folders

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 // this component handles the App template used on every page.
 import React, {PropTypes} from 'react';
 import Header from './common/Header';
+import Footer from './common/Footer';
 
 class App extends React.Component{
   render(){
     return(
-      <div className="container-fluid">
+      <div id="wrap" className="container-fluid">
         <Header />
         {this.props.children}
+        <Footer />
       </div>
     );
   }
--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -3,12 +3,28 @@ import React from 'react';
 class AboutPage extends React.Component{
   render(){
     return(
-      <div>
-        <h1>About Page</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tristique, eros vitae dignissim dictum, metus massa elementum neque, sed congue nisl felis a libero. Vivamus porta, neque at ultrices vulputate, augue ipsum blandit nulla, sed malesuada neque ex ullamcorper orci. Integer auctor, ex at pulvinar blandit, massa nisi malesuada dolor, vitae accumsan nibh odio in ligula. Quisque eget sagittis purus, quis sodales arcu. Pellentesque hendrerit malesuada erat eu vehicula. Curabitur rutrum justo id arcu aliquet rhoncus. Fusce sit amet magna eu orci fermentum congue. Suspendisse posuere est odio, et hendrerit mauris rhoncus ut. Phasellus nec elit sem. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec mattis euismod ligula nec tempor. Sed sit amet nibh risus. Morbi urna risus, pulvinar sit amet diam sed, aliquet laoreet mi. Nam malesuada volutpat est, sed ullamcorper leo cursus quis. Nunc non turpis dignissim, auctor eros sit amet, condimentum arcu. Curabitur egestas ante ut varius varius.</p>
-        <p>Suspendisse semper tortor sed orci tempus, sed iaculis erat scelerisque. In lorem sapien, consectetur gravida laoreet vel, tincidunt eu lacus. In eget est ut arcu sodales aliquam. Donec tincidunt interdum erat, id sollicitudin diam vehicula id. Integer aliquam tortor ac lectus placerat, vel iaculis felis pharetra. Nam placerat eget ante at vehicula. Integer quis enim vitae sapien viverra aliquam at sed sapien. Pellentesque id aliquet nisl. Quisque consequat mi a interdum bibendum. Mauris vel ornare dui. Mauris auctor mi tortor, id eleifend elit ullamcorper vel. Quisque dictum turpis sed est iaculis efficitur. Sed sit amet elit purus.</p>
-        <p>Iteger finibus lacinia sem, mollis pellentesque nunc finibus vel. Aliquam sed ex leo. Maecenas posuere eu mauris a tempus. Proin id dolor eros. Nulla eleifend quam condimentum dui condimentum sagittis. Aenean ac elit turpis. Praesent et lacus sollicitudin, suscipit elit eu, venenatis libero. Sed condimentum lectus ut justo fermentum dictum. Donec id arcu eu neque placerat auctor. Vestibulum aliquet leo vel mauris vulputate rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel nisl tellus. Duis sagittis, elit ut elementum auctor, est neque tempus leo, tincidunt condimentum odio ante sit amet ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </div>
+      <section id="about" className="offset section">
+        <div className="lightGrey">
+          <div className="container clearfix">
+            <div className="intro">
+                <h1 className="titleBig">About Page</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                   magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                   Ut enim ad minim veniam, quis nostrud exercitation .</p>
+              </div>
+              <div className="sixteen columns">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lectus sem, finibus sit amet suscipit in, cursus id erat. Sed feugiat rutrum dictum. Vestibulum dapibus sapien id elit bibendum, a euismod ipsum commodo. Integer at nisl non ligula ultrices volutpat sed vitae arcu. Proin placerat iaculis augue, interdum pharetra felis auctor sit amet. Pellentesque rutrum, elit non feugiat porttitor, felis est bibendum dui, nec commodo felis quam ut neque. Morbi commodo neque in imperdiet efficitur. Maecenas sem urna, molestie fermentum finibus sit amet, pretium ut purus. Vivamus justo ex, egestas eu ullamcorper in, volutpat eu justo. Etiam vestibulum eros a nunc molestie, et efficitur lorem faucibus. Vestibulum at erat tempus, condimentum dui euismod, gravida lorem. Morbi et justo eros.
+                  </p>
+                  <p>Maecenas vitae sapien dui. Duis tincidunt purus ut tortor sodales pulvinar. Aliquam finibus arcu massa. Maecenas fermentum, tellus nec hendrerit finibus, felis nunc fermentum eros, sit amet convallis mi arcu ut arcu. Sed quis sapien tincidunt, tristique urna eget, ultricies neque. Aliquam tristique at mi quis volutpat. Aenean aliquet sed ligula a gravida. Suspendisse potenti. Phasellus in leo est. Suspendisse pharetra elementum lobortis. Fusce sed mauris dignissim nisl aliquet pulvinar at ut nisi.
+                  </p>
+                  <p>Aenean congue mauris id libero aliquam, vitae iaculis magna imperdiet. Donec vulputate, nisi eu laoreet tempor, mauris quam luctus tortor, et tempor mauris diam in ex. Nulla eu massa nibh. Vivamus non sollicitudin mi, et pretium urna. Vivamus finibus ultricies quam quis rhoncus. Integer pharetra lobortis elementum. Quisque ac malesuada purus. Pellentesque tincidunt ex dapibus, congue mi ut, pulvinar leo. Pellentesque pharetra nulla sit amet tortor ornare tristique. Donec erat ex, gravida et commodo et, gravida et neque. Curabitur vehicula lectus est, ut faucibus ligula pretium nec. Sed semper sagittis mi, sit amet hendrerit mauris varius eu. Nullam vel commodo purus. Nulla maximus urna dolor, eu venenatis elit auctor nec. Duis aliquet enim at lacinia venenatis.
+                  </p>
+                  <p>Ut lacus massa, ullamcorper nec consectetur vitae, aliquam eget tortor. Proin sagittis ligula elit, in pulvinar dui molestie ut. Sed ante mi, pharetra at egestas ut, cursus in magna. Phasellus non rhoncus tellus. Mauris consectetur, erat vitae posuere gravida, ante nisi faucibus urna, ut pretium velit ex sed lorem. Pellentesque semper ac mi at aliquet. Vivamus suscipit ultricies quam et scelerisque. Phasellus vel quam a erat consectetur commodo. Ut quis est odio. Curabitur in dui aliquam, pretium sem a, porta est. Aliquam volutpat libero ipsum, quis condimentum justo cursus sed. Proin sit amet euismod nisl. Etiam vitae sapien eu orci vehicula auctor. Sed aliquet tristique elit, sit amet fermentum velit feugiat ut. Duis sodales, risus eu hendrerit interdum, lectus arcu euismod lacus, non pulvinar enim libero sed tellus.
+                  </p>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
